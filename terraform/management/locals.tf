c15efc4934b96ec35c62ada81b24ae501827a8c5
Gerhard Keuck <hello@gerhardkeuck.com>
2026-05-18 19:40:00 +0200

    fix: iam access due to invalid sub
    Using wildcard for now.

--- terraform/management/locals.tf
+++ terraform/management/locals.tf
@@ -2,7 +2,7 @@ locals {
   environment = terraform.workspace
   name_prefix = "${var.service}-${local.environment}"
 
-  github_oidc_sub          = coalesce(var.github_oidc_sub, "repo:${var.github_repository}:ref:refs/heads/*")
+  github_oidc_sub          = coalesce(var.github_oidc_sub, "repo:${var.github_repository}:*")
   github_actions_role_name = "${local.name_prefix}-github-actions"
 
   terraform_state_bucket_arn = "arn:${data.aws_partition.current.partition}:s3:::${var.terraform_state_bucket}"
